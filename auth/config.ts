7f5c60ae73c8eccd30dc0128ab541a7c8eb00cf7
Jean-Luc Martin <j.martin@glossgenius.com>
2023-11-06 18:06:53 -0600

    repro of apikey issue in email verification

--- auth/config.ts
+++ auth/config.ts
@@ -1,12 +1,9 @@
 export const firebaseConfig = {
-  // Your web app's Firebase configuration here
-  // See https://firebase.google.com/docs/web/setup#add-sdks-initialize
-  apiKey: 'API_KEY',
-  authDomain: 'PROJECT_ID.firebaseapp.com',
-  databaseURL: 'https://PROJECT_ID.firebaseio.com',
-  projectId: 'PROJECT_ID',
-  storageBucket: 'PROJECT_ID.appspot.com',
-  messagingSenderId: 'SENDER_ID',
-  appId: 'APP_ID',
-  measurementId: 'G-MEASUREMENT_ID',
+  apiKey: 'AIzaSyCFgV_DG9S5cShnDxc52GsOfvdgz9FM28w',
+  authDomain: 'biteplan-c6eba.firebaseapp.com',
+  projectId: 'biteplan-c6eba',
+  storageBucket: 'biteplan-c6eba.appspot.com',
+  messagingSenderId: '403685275374',
+  appId: '1:403685275374:web:ee4404acdcce34163925ff',
+  measurementId: 'G-LSE3JLR0X0',
 };
